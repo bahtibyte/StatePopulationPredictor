0f1df32d54919c75b601a11520170d4b389eb7e8
bahti-centrol <bahti@centrol.io>
2022-03-26 15:25:06 -0400

    preperaded the data

--- process.py
+++ process.py
@@ -1,21 +1,32 @@
 import csv
 
+states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 
+        'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 
+        'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 
+        'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 
+        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 
+        'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 
+        'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+
+transfers = {}
+rates = {}
+edges = {}
+
+for start in states:
+    transfers[start], rates[start], edges[start] = {}, {}, {}
+    for end in states:
+        transfers[start][end] = {}
+        rates[start][end] = {}
+        edges[start][end] = {}
+
 
 for year in range(2010, 2020):
-    data = {}
-
     with open(f'data/table_{year}.csv', 'r') as file:
         reader = csv.reader(file)
         fields = next(reader)
         usa = next(reader) # ignore the data about usa stats
 
         population = {}
-        data[year] = {}
-        edges = data[year]
-
-        for i in range(4, len(fields)):
-            edges[fields[i]] = {}  # Creates a empty edge for all states available
-
         for row in reader:
             population[row[0]] = row[1]
 
@@ -27,19 +38,23 @@ for year in range(2010, 2020):
         best_start = None
         best_end = None
 
+        print()
         for start in edges:
             for end in edges[start]:
-                edges[start][end] = {
-                    'value': edges[start][end],
-                    'rate': int(edges[start][end]) * 1.0 / int(population[start])
-                }
+                rates[start][end][year] = int(edges[start][end]) * 1.0 / int(population[start])
+                transfers[start][end][year] = edges[start][end]
 
-                if edges[start][end]['rate'] > highest:
-                    highest = edges[start][end]['rate']
+                if rates[start][end][year] > highest:
+                    highest = rates[start][end][year]
                     best_start = start
                     best_end = end
 
-        print('year',year,'result:',best_start,'to',best_end,'rate of change of',highest, 'total left:',edges[best_start][best_end]['value'])
+        print('year',year,'result:',best_start,'to',best_end,'rate of change of',highest, 'total left:',transfers[best_start][best_end][year])
 
 
+for year in range(2010, 2020):
+    x = 'Nevada'
+    y = 'Texas'
+
+    print(f"{transfers[x][y][year]} people left {x} to go to {y} during {year}. The rate of ppl leaving {x} was {rates[x][y][year]}")
 
